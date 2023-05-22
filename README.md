084aac1cc2408c191ee7d40d89e6d5b1fcf217fa
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-05-22 02:03:04 -0700

    start readme file

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Retail-roBought
 
-##
+## Description 
+Retail-roBought is the back-end for an e-commerce site. This Express.js API uses Sequelize to interact with a MySQL database. 
+
+## Table of Contents
+- [Screenshots](#screenshots)
+- [Walk-Through-Video] (#walk-through-video)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Screenshots
+
+## Walk-Through-Video
+
+## Usage 
+
+## Credits 
+
+## License 
+
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,6 +24,12 @@ router.get("/:id", async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -103,19 +109,19 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
     const product = await Product.destroy({
-      where: { id: req.params.id }
+      where: { id: req.params.id },
     });
 
     if (!product) {
-      res.status(404).json({ message: 'No product found with this id!' });
+      res.status(404).json({ message: "No product found with this id!" });
       return;
     }
 
-    res.status(200).json({ message: 'Product deleted successfully.' });
+    res.status(200).json({ message: "Product deleted successfully." });
   } catch (err) {
     res.status(500).json(err);
   }
