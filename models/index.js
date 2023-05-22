371b099246c4fdc5ee1a407dbea5c819c0c8de58
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-05-22 01:30:50 -0700

    updated models index.js file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: "category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  through: ProductTag,
+  onDelete:"cascade"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  onDelete: "cascade"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  onDelete: "cascade"
+})
+
 
 module.exports = {
   Product,
